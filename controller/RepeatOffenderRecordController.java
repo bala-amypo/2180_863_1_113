7204a140c214fe0028710fe286d6cb26d48619df
bala-amypo <bala@amypo.in>
2025-12-18 16:28:21 +0000

    User commit

---
+++ controller/RepeatOffenderRecordController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/repeat-offenders")
+public class RepeatOffenderRecordController {
+}
