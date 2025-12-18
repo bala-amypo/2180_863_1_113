916de9dc20978144957168b806879aa5b2bdf5f3
bala-amypo <bala@amypo.in>
2025-12-18 16:25:21 +0000

    User commit

---
+++ controller/EvidenceRecordController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/evidence")
+public class EvidenceRecordController {
+}
---
+++ controller/PenaltyActionController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/penalties")
+public class PenaltyActionController {
+}
