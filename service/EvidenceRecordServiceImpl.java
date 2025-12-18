699832cda020de09b0889aa08d1f72636e4cf9b8
bala-amypo <bala@amypo.in>
2025-12-18 16:19:21 +0000

    User commit

---
+++ service/EvidenceRecordServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.*;
+
+public class EvidenceRecordServiceImpl {
+
+    public EvidenceRecordServiceImpl(
+            EvidenceRecordRepository evidenceRepo,
+            IntegrityCaseRepository caseRepo
+    ) {}
+}
---
+++ service/PenaltyActionServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.*;
+
+public class PenaltyActionServiceImpl {
+
+    public PenaltyActionServiceImpl(
+            PenaltyActionRepository penaltyRepo,
+            IntegrityCaseRepository caseRepo
+    ) {}
+}
