f5d78a50358c887c204edec1aa70b14e10015319
bala-amypo <bala@amypo.in>
2025-12-21 08:58:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/EvidenceRecordRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.EvidenceRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvidenceRecordRepository extends JpaRepository<EvidenceRecord, Long> {
+}
