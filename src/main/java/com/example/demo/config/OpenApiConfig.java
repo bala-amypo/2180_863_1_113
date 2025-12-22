75a2399544a8800cbb1256747dfaf884755c8dd6
bala-amypo <bala@amypo.in>
2025-12-22 03:39:37 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
 
 @Configuration
 public class OpenApiConfig {
@@ -11,9 +14,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Academic Integrity Case Tracker API")
-                        .version("1.0")
-                        .description("Digital Queue Management System"));
+               .servers(List.of(
+                new Server().url("https://9659.pro604cr.amypo.ai/")
+               ));
     }
 }
