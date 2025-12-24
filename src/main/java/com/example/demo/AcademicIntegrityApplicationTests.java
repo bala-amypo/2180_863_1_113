a52e9eb6dcbaa27d529bdde69cb96c82bbdbaccc
bala-amypo <bala@amypo.in>
2025-12-24 04:56:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/AcademicIntegrityApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class AcademicIntegrityApplicationTests {
+
+}
